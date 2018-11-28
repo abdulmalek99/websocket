798f6f2a0b1d43ef7d0e850bee9dc7f7ac78876a
Abdulmalek Muthana Abdulmalek <9905abab@res.ludvika.intra>
2018-11-28 10:26:43 +0100

    löst uppgiften

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,36 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
+import password from './config';
+
+const connection = new WebSocket('ws://104.248.143.87:1337');
+
+connection.onopen = () => {
+  console.log('Uppkoppling started ... ');
+};
+connection.onerror = error => {
+  console.log(`Èrror: ${error}`);
+};
+connection.onmessage = message => {
+  const textEl = document.getElementById('logger');
+  const obj = JSON.parse(message.data);
+  if (obj.type === 'heartbeat') {
+    textEl.textContent += `${obj.data}, `;
+  }
+};
+connection.onclose = () => {
+  console.log('Uppkoppling nedstängd ... ');
+};
+
+const btnEl = document.getElementById('btn');
+const textEl = document.getElementById('logger');
+btnEl.addEventListener("click", () => {
+  const obj = {
+    type: 'heartbeat',
+    data: '',
+    key: password
+  };
+  const jsonObj = JSON.stringify(obj);
+  connection.send(jsonObj);
+});
+
